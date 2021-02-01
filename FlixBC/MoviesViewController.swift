f872ff172e2411e69651d256c4f438ef0fff5e34
Brayam Corral Flores <brayamcorral@gmail.com>
2021-01-31 21:20:02 -0800

    Added movie posters. Base app is finished.

--- FlixBC/MoviesViewController.swift
+++ FlixBC/MoviesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
         
@@ -49,15 +50,22 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         // if a cell is off screen, recycle that cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
-        
+        // get individual movie API content and store in movie variable
         let movie = movies[indexPath.row]
-        
+        // stores title of movie
         let title = movie["title"] as! String
+        // stores description of movie
         let synopsis = movie["overview"] as! String
         
+        // Update labels on app
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
         
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.PosterView.af_setImage(withURL: posterUrl!)
         return cell
     }
     
