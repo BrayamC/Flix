8d495b21244a4a28d1089b32d10578f9a754a5f6
Brayam Corral Flores <brayamcorral@gmail.com>
2021-02-08 17:08:58 -0800

    Youtube video no loads

--- FlixBC/MovieGridVideoViewController.swift
+++ FlixBC/MovieGridVideoViewController.swift
@@ -6,31 +6,46 @@
 //
 
 import UIKit
+import youtube_ios_player_helper
 
 class MovieGridVideoViewController: UIViewController {
 
     var movie: [String:Any]!
+    var videos = [[String:Any]]()
+    
+    @IBOutlet weak var playerView: YTPlayerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print(movie)
-        print("*************")
-        
+        var videoKey:String = ""
+        // Do any additional setup after loading the view.
+        let APIUrl1 = "https://api.themoviedb.org/3/movie/"
         let movieID = movie["id"] as! Int
-        print(movieID)
+        let APIUrl2 = "/videos?api_key=8f46543c2255c7b5ee4ec102a5005c02"
         
-    }
+        let APIUrlTotal = APIUrl1 + "\(movieID)" + APIUrl2
+        //print(APIUrlTotal)
+        
+        let url = URL(string: APIUrlTotal)!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+              let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+
+            self.videos = dataDictionary["results"] as! [[String:Any]]
+            
+            videoKey = self.videos[0]["key"] as! String ?? ""
+            self.playerView.load(withVideoId: videoKey)
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+           }
+        }
+        task.resume()
+    
     }
-    */
 
 }
